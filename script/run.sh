ba21b799cc49eb3a816dd296360d03a5bf4a27b5
Vladmir Parshikov <v-vparshikov@expedia.com>
2015-04-09 17:11:57 +0100

    updated script

--- script/run.sh
+++ script/run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-cd $1
+
+DIR = $1
+cd $DIR
+. props.cfg
 
 kill -9 `cat pid`
 rm -f adam.jar
 mv *SNAPSHOT*.jar adam.jar
 
-nohup java -jar adam.jar --email=$2 --password=$3 > out.log &
+java -jar adam.jar --facebook.email=$fb.email --facebook.password=$fb.pass > out.log &
 PID=$!
 echo "$PID" > pid
