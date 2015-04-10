3c85a89ecfbf9e7c32b73b876dd91c5335fe9d4f
Vladmir Parshikov <v-vparshikov@expedia.com>
2015-04-10 11:59:43 +0100

    log4j

--- script/run.sh
+++ script/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cd $1
+cd .
 . props.cfg
 
 kill -9 `cat pid`
 rm -f adam.jar
 mv *SNAPSHOT*.jar adam.jar
 
-java -jar adam.jar --facebook.email=$fb.email --facebook.password=$fb.pass > out.log &
+java -jar adam.jar --facebook.email=$fb_email --facebook.password=$fb_pass > out.log &
 PID=$!
 echo "$PID" > pid
