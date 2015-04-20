4537e895dd689a7f1d246c50dbd638742856b52e
Vladmir Parshikov <v-vparshikov@expedia.com>
2015-04-20 18:22:22 +0100

    some debugging

--- src/main/java/com/adam/controller/HelloController.java
+++ src/main/java/com/adam/controller/HelloController.java
@@ -4,12 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Date;
+
 @Controller
 public class HelloController {
+
+    private Date started = new Date();
 
     @RequestMapping("/")
     @ResponseBody
     public String index() {
-        return "Hello, my name is Adam!";
+        return "Hello, my name is Adam! I was started: " + started;
     }
 }
